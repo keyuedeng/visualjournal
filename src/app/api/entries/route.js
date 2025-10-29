6f9325f6493a0a7c358beb1ab77f521c1783f997
keyuedeng <keyuedeng6@gmail.com>
2025-10-29 15:14:57 -0700

    implemented GET, POST, DELETE api routes

---
+++ src/app/api/entries/route.js
@@ -0,0 +1,49 @@
+import { prisma } from '@/lib/prisma' // so we can use it in our code
+
+export async function POST(request) {
+    try {
+        const data = await request.json() //await used in async function to pause the execution of the function until a promise settles
+        const savedEntry = await prisma.entry.create({
+            data: {
+                title: data.title,
+                body: data.body,
+            },
+        })
+        return Response.json({ message: "Entry created successfully", entry: savedEntry }) 
+    } catch (error) {
+        console.error("Failed to save entry:", error)
+        return Response.json({ error: "Failed to save entry" }, { status: 500 })
+    }
+}
+
+export async function GET() {
+    try {
+        const entries = await prisma.entry.findMany()
+        return Response.json(entries)
+    }
+    catch (error) {
+        console.error("Failed to fetch entries:", error)
+        return Response.json({ error: "Failed to fetch entries" }, {status: 500 })
+    }
+}
+
+export async function DELETE(request) {
+    try {
+        const { searchParams } = new URL(request.url)
+        const id = searchParams.get("id")
+
+        if (!id) {
+            return Response.json({ error: "Missing entry ID" }, { status: 400 })
+        }
+
+        await prisma.entry.delete({
+            where: { id: id},
+        })
+
+        return Response.json({ message: "Entry deleted successfully" })
+    }
+    catch (error) {
+        console.error("Failed to delete entry:", error)
+        return Response.json({ error: "Failed to delete entry" }, { status: 500 })
+    }
+}
---
+++ src/lib/prisma.js
@@ -0,0 +1,23 @@
+/* 
+this file will hold a single Prisma client instance that the whole app can share
+- creating a new prisma client every time a request is made is slow
+- instead, this is a "connection manager" and can import it anywhere u need database access
+- basically the app's database connector
+*/
+
+import { PrismaClient } from '../generated/prisma'
+
+let prisma
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient({
+      log: ['query', 'error', 'warn'],
+    })
+  }
+  prisma = global.prisma
+}
+
+export { prisma }
