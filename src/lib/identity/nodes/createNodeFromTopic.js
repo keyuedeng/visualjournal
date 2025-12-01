3f7ea8f75fb51ba1198b648af6de2fa4528b5523
keyuedeng <keyuedeng6@gmail.com>
2025-11-30 21:54:29 -0800

    feat: implement node creation

---
+++ src/lib/identity/nodes/createNodeFromTopic.js
@@ -0,0 +1,23 @@
+import { prisma } from "@/lib/prisma";
+
+/*
+creates canonical node using topicalias
+input: alias (topic as string, embedding as vector, userId)
+*/
+
+export async function createNodeFromTopic(alias) {
+    const { userId, topic, embedding } = alias
+    const label = topic
+    const newNode = await prisma.node.create({
+        data: {
+            userId, 
+            label, 
+            category: "unknown",
+            count: 1,
+            embedding,
+            contexts: [],
+        }
+    })
+
+    return newNode;
+}
