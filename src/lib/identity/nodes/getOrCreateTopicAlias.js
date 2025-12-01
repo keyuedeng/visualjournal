d483f9218a2a6440d062ffff8c1e8f825e41ab74
keyuedeng <keyuedeng6@gmail.com>
2025-11-30 20:38:58 -0800

    feat: add topic alias creation logic

--- src/lib/identity/nodes/getOrCreateTopicAlias.js
+++ src/lib/identity/nodes/getOrCreateTopicAlias.js
@@ -3,15 +3,33 @@ import { embedText } from "../embeddings/embedText"
 import { normaliseTopic } from "./normaliseTopic"
 
 export async function getOrCreateTopicAlias(userId, rawTopic) {
+    if (!rawTopic) {
+        throw new Error("getOrCreateTopicAlias: rawTopic is requireed")
+    }
+
     // normalise raw topic string
     const topic = normaliseTopic(rawTopic)
 
     // check if this normalised topic exists in schema already
-    prisma.TopicAlias
-        // if exists, return this alias
-
-    // if not, then create topic alias
-        //get embedding
-        // insert alias into DB
-        // return new alias
+    const existing = await prisma.topicAlias.findFirst({
+        where: {
+            userId: userId, 
+            topic: topic
+        }
+    })
+    // if exists, return this alias
+    if (existing) {
+        return existing
+    }
+    const embedding = await embedText(topic)
+    alias = await prisma.topicAlias.create({
+        data: {
+            userId,
+            topic,
+            embedding,
+            canonicalNodeId: null // ASSIGN LATER DURING CANONICALISATION
+        }
+    })
+    
+    return alias
 }
